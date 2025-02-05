8bc992d4d10533094c8575bf1a37b6c584aa5e47
FleecyBear <nikusha.kk1990@gmail.com>
2025-02-05 16:17:17 +0400

    styles: header, footer

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -7,7 +7,8 @@ const Header: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className="bg-gradient-to-r from-teal-500 to-blue-600 text-white p-4 fixed top-0 left-0 right-0 z-50">
+    <header className="bg-gradient-to-r  from-[#d83F87] to-[#2a1b3c] 
+    dark:from-[#44318d] dark:to-[#2a1b3c] text-white p-4 fixed top-0 left-0 right-0 z-50">
       <div className="container mx-auto flex justify-between items-center">
         <button className="lg:hidden text-xl" onClick={() => setMenuOpen(!menuOpen)}>
           {menuOpen ? '✖️' : '☰'}
@@ -28,7 +29,7 @@ const Header: React.FC = () => {
         <div className="flex space-x-4">
           <Link href="/cart" className="hover:text-gray-300">Cart</Link>
           <Link href="/profile" className="hover:text-gray-300">Profile</Link>
-          <button className="bg-blue-700 px-4 py-2 rounded hover:bg-blue-800">
+          <button className="bg-[#44318d] dark:bg-[#e98074] px-4 py-2 rounded hover:bg-[#e98074] dark:hover:bg-[#44318d]">
             <Link href="/login">Login</Link>
           </button>
         </div>
